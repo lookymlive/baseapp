e54eb7876caf9e705b8bca0f61f646bc0a196087
Lookym <lookymlive@gmail.com>
2024-07-30 19:01:16 -0300

    Config PORT .env

--- server/index.js
+++ server/index.js
@@ -1,15 +1,11 @@
-import express from 'express';
+import express from "express";
+import { PORT } from "./config.js";
+import postsRoutes from "./routes/posts.routes.js";
+import { connectDB } from "./db.js";
 
-import postsRoutes from './routes/posts.routes.js';
-import { connectDB } from './db.js';
+const app = express();
+connectDB();
+app.use(postsRoutes);
 
-const app = express()
-
-connectDB( )
-app.use(postsRoutes)
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
