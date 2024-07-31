601278e20e70731aeefb1c28d8de659c309f79f6
Lookym <lookymlive@gmail.com>
2024-07-31 15:12:41 -0300

    uploadfile an cloudinary add

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import fileUpload from "express-fileupload";
 import postsRoutes from "./routes/posts.routes.js";
+
+
 
 const app = express();
 
 //Middlewares
 app.use(express.json());
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: "./uploadFiles",
+}));
 
 //Esto es una ruta
 app.use(postsRoutes);
--- server/controllers/posts.controller.js
+++ server/controllers/posts.controller.js
@@ -1,36 +1,59 @@
-import Post from '../models/Post.js'
-
+import Post from "../models/Post.js";
 
 export const getPosts = async (req, res) => {
-   const posts = await Post.find()
-  res.send(posts)
-}
+  try {
+    const posts = await Post.find();
+    res.send(posts);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500).json({ message: error.message });
+  }
+};
 
 export const createPost = async (req, res) => {
-  const {title, description} = req.body
-  const newPost = new Post({title, description})
-  await  newPost.save()
-return res.json(newPost)
-}
+  try {
+    const { title, description } = req.body;
+    if (req.files.image) {
+      
+
+      return res.sendStatus(400);
+    }
+    const newPost = new Post({ title, description });
+    await newPost.save();
+    return res.json(newPost);
+  } catch (error) {
+    return res.sendStatus(500).json({ message: error.message });
+  }
+};
 
 export const updatePost = async (req, res) => {
-console.log(req.body) 
-console.log(req.params)
- const post = await Post.updateOne({_id :req.params.id}, req.body, {new: true}) 
- console.log(post)
-return res.send('received a put request')
-}
+  try {
+    const updatedpost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    console.log(post);
+    return res.send(updatedpost);
+  } catch (error) {
+    return res.Status(500).json({ message: error.message });
+  }
+};
 export const deletePost = async (req, res) => {
-  const postRemoved = await Post.findByIdAndDelete(req.params.id)
- if(postRemoved) return res.sendStatus(404)
- else return res.send(204)
+  try {
+    const postRemoved = await Post.findByIdAndDelete(req.params.id);
+    if (postRemoved) return res.sendStatus(404);
+    else return res.send(204);
+  } catch (error) {
+    return res.Status(500).json({ message: error.message });
+  }
+};
+export const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.sendStatus(404);
+    return res.json(post);
+  } catch (error) {
+    return res.Status(500).json({ message: error.message });
+  }
 
-}
-export const getPost= async (req, res) => {
-  const post = await Post.findById(req.params.id)
-  if(!post) return res.sendStatus(404)
-    return res.json(post)
-
-
-  res.send('getting a post')
-}
+  res.send("getting a post");
+};
